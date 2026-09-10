e915fde3c4df81f991d9728a51f622e0cea1f698
Rianna Cantarelli <rianna.aeon@gmail.com>
2026-09-09 23:37:20 -0300

    fix: register missing XERR Codes

--- pkg/xdb/pkg_xerrors.go
+++ pkg/xdb/pkg_xerrors.go
@@ -25,37 +25,40 @@ const (
 	// Format expects: TARGET, DETAILS
 	XERR_MIGRATION_EXECUTION_FAILED xerrors.ErrorCode = "E1003"
 
-	XERR_REPO_INSERT_WITH_UNEXPECTED_NUMERIC_PK xerrors.ErrorCode = "E0002"
-	XERR_REPO_INSERT_WITH_UNEXPECTED_STRING_PK  xerrors.ErrorCode = "E0003"
-	XERR_REPO_INSERT_WITH_UNEXPECTED_EMPTY_PK   xerrors.ErrorCode = "E0004"
-	XERR_REPO_INSERT_WITH_UNEXPECTED_NIL_PK     xerrors.ErrorCode = "E0005"
-	XERR_REPO_INSERT_EXECUTION_FAILED           xerrors.ErrorCode = "E0006"
-	XERR_REPO_INSERT_FETCH_ID_FAILED            xerrors.ErrorCode = "E0007"
-	XERR_REPO_UPDATE_INVALID_NUMERICAL_PK       xerrors.ErrorCode = "E0008"
-	XERR_REPO_UPDATE_EMPTY_STRING_PK            xerrors.ErrorCode = "E0009"
-	XERR_REPO_UPDATE_PK_NIL                     xerrors.ErrorCode = "E0010"
-	XERR_REPO_UPDATE_UNKNOWN_PK_TYPE            xerrors.ErrorCode = "E0011"
-	XERR_REPO_UPDATE_NO_COLUMNS_DEFINED         xerrors.ErrorCode = "E0012"
-	XERR_REPO_UPDATE_EXEC_FAILED                xerrors.ErrorCode = "E0013"
-	XERR_REPO_UPDATE_VERIFY_ROWS_FAILED         xerrors.ErrorCode = "E0014"
-	XERR_REPO_UPDATE_RECORD_NOT_FOUND           xerrors.ErrorCode = "E0015"
-	XERR_REPO_DELETE_EXEC_FAILED                xerrors.ErrorCode = "E0016"
-	XERR_REPO_DELETE_VERIFY_ROWS_FAILED         xerrors.ErrorCode = "E0017"
-	XERR_REPO_DELETE_RECORD_NOT_FOUND           xerrors.ErrorCode = "E0018"
-	XERR_REPO_INTERFACE_ASSERTION_FAILED        xerrors.ErrorCode = "E0019"
-	XERR_REPO_GET_BY_ID_EXEC_FAILED             xerrors.ErrorCode = "E0020"
-	XERR_REPO_GET_BY_ID_RECORD_NOT_FOUND        xerrors.ErrorCode = "E0021"
-	XERR_REPO_GET_BY_ID_SCAN_FAILED             xerrors.ErrorCode = "E0022"
-	XERR_REPO_GET_ALL_EXEC_FAILED               xerrors.ErrorCode = "E0023"
-	XERR_REPO_GET_ALL_SCAN_FAILED               xerrors.ErrorCode = "E0024"
-	XERR_REPO_GET_ALL_ITERATION_FAILED          xerrors.ErrorCode = "E0025"
-	XERR_REPO_GET_BY_FIELD_EXEC_FAILED          xerrors.ErrorCode = "E0026"
-	XERR_REPO_GET_BY_FIELD_SCAN_FAILED          xerrors.ErrorCode = "E0027"
-	XERR_REPO_GET_WHERE_ARGS_MISMATCH           xerrors.ErrorCode = "E0028"
-	XERR_REPO_GET_WHERE_EXEC_FAILED             xerrors.ErrorCode = "E0029"
-	XERR_REPO_GET_WHERE_SCAN_FAILED             xerrors.ErrorCode = "E0030"
-	XERR_REPO_QUERY_RAW_EXEC_FAILED             xerrors.ErrorCode = "E0031"
-	XERR_REPO_QUERY_RAW_SCAN_FAILED             xerrors.ErrorCode = "E0032"
+	XERR_REPO_INSERT_WITH_UNEXPECTED_NUMERIC_PK xerrors.ErrorCode = "E2001"
+	XERR_REPO_INSERT_WITH_UNEXPECTED_STRING_PK  xerrors.ErrorCode = "E2002"
+	XERR_REPO_INSERT_WITH_UNEXPECTED_EMPTY_PK   xerrors.ErrorCode = "E2003"
+	XERR_REPO_INSERT_WITH_UNEXPECTED_NIL_PK     xerrors.ErrorCode = "E2004"
+	XERR_REPO_INSERT_EXECUTION_FAILED           xerrors.ErrorCode = "E2005"
+	XERR_REPO_INSERT_FETCH_ID_FAILED            xerrors.ErrorCode = "E2006"
+
+	XERR_REPO_UPDATE_INVALID_NUMERICAL_PK xerrors.ErrorCode = "E3001"
+	XERR_REPO_UPDATE_EMPTY_STRING_PK      xerrors.ErrorCode = "E3002"
+	XERR_REPO_UPDATE_PK_NIL               xerrors.ErrorCode = "E3003"
+	XERR_REPO_UPDATE_UNKNOWN_PK_TYPE      xerrors.ErrorCode = "E3004"
+	XERR_REPO_UPDATE_NO_COLUMNS_DEFINED   xerrors.ErrorCode = "E3005"
+	XERR_REPO_UPDATE_EXEC_FAILED          xerrors.ErrorCode = "E3006"
+	XERR_REPO_UPDATE_VERIFY_ROWS_FAILED   xerrors.ErrorCode = "E3007"
+	XERR_REPO_UPDATE_RECORD_NOT_FOUND     xerrors.ErrorCode = "E3008"
+
+	XERR_REPO_DELETE_EXEC_FAILED        xerrors.ErrorCode = "E4001"
+	XERR_REPO_DELETE_VERIFY_ROWS_FAILED xerrors.ErrorCode = "E4002"
+	XERR_REPO_DELETE_RECORD_NOT_FOUND   xerrors.ErrorCode = "E4003"
+
+	XERR_REPO_GET_BY_ID_EXEC_FAILED      xerrors.ErrorCode = "E5001"
+	XERR_REPO_GET_BY_ID_RECORD_NOT_FOUND xerrors.ErrorCode = "E5002"
+	XERR_REPO_GET_BY_ID_SCAN_FAILED      xerrors.ErrorCode = "E5003"
+	XERR_REPO_GET_ALL_EXEC_FAILED        xerrors.ErrorCode = "E5004"
+	XERR_REPO_GET_ALL_SCAN_FAILED        xerrors.ErrorCode = "E5005"
+	XERR_REPO_GET_ALL_ITERATION_FAILED   xerrors.ErrorCode = "E5006"
+	XERR_REPO_GET_BY_FIELD_EXEC_FAILED   xerrors.ErrorCode = "E5007"
+	XERR_REPO_GET_BY_FIELD_SCAN_FAILED   xerrors.ErrorCode = "E5008"
+	XERR_REPO_GET_WHERE_ARGS_MISMATCH    xerrors.ErrorCode = "E5009"
+	XERR_REPO_GET_WHERE_EXEC_FAILED      xerrors.ErrorCode = "E5010"
+	XERR_REPO_GET_WHERE_SCAN_FAILED      xerrors.ErrorCode = "E5011"
+
+	XERR_REPO_QUERY_RAW_EXEC_FAILED xerrors.ErrorCode = "E6001"
+	XERR_REPO_QUERY_RAW_SCAN_FAILED xerrors.ErrorCode = "E6002"
 )
 
 // xerrorDomainMapRegistry centralizes the core validation error metadata block and default
@@ -76,104 +79,178 @@ var xerrorDomainMapRegistry = map[xerrors.ErrorCode]xerrors.MetaMessage{
 		"",
 		[]string{"TARGET", "DETAILS"},
 	),
+
+	//
+	//
+
+	XERR_REPO_INSERT_WITH_UNEXPECTED_NUMERIC_PK: xerrors.NewMetaMessage(
+		"cannot insert entity with an existing numerical ID",
+		"",
+		[]string{},
+	),
+	XERR_REPO_INSERT_WITH_UNEXPECTED_STRING_PK: xerrors.NewMetaMessage(
+		"cannot insert entity with an existing primary key string",
+		"",
+		[]string{},
+	),
+	XERR_REPO_INSERT_WITH_UNEXPECTED_EMPTY_PK: xerrors.NewMetaMessage(
+		"cannot insert entity: natural primary key string cannot be empty",
+		"",
+		[]string{},
+	),
+	XERR_REPO_INSERT_WITH_UNEXPECTED_NIL_PK: xerrors.NewMetaMessage(
+		"cannot insert entity: natural primary key cannot be nil",
+		"",
+		[]string{},
+	),
+	XERR_REPO_INSERT_EXECUTION_FAILED: xerrors.NewMetaMessage(
+		"failed to execute insert statement in the database",
+		"",
+		[]string{},
+	),
+	XERR_REPO_INSERT_FETCH_ID_FAILED: xerrors.NewMetaMessage(
+		"failed to retrieve last inserted numerical ID from database",
+		"",
+		[]string{},
+	),
+
+	//
+	//
+
+	XERR_REPO_UPDATE_INVALID_NUMERICAL_PK: xerrors.NewMetaMessage(
+		"cannot update entity with invalid numerical ID (must be > 0)",
+		"",
+		[]string{},
+	),
+	XERR_REPO_UPDATE_EMPTY_STRING_PK: xerrors.NewMetaMessage(
+		"cannot update entity with empty string key",
+		"",
+		[]string{},
+	),
+	XERR_REPO_UPDATE_PK_NIL: xerrors.NewMetaMessage(
+		"cannot update entity without a primary key",
+		"",
+		[]string{},
+	),
+	XERR_REPO_UPDATE_UNKNOWN_PK_TYPE: xerrors.NewMetaMessage(
+		"unknown primary key type format",
+		"",
+		[]string{},
+	),
+	XERR_REPO_UPDATE_NO_COLUMNS_DEFINED: xerrors.NewMetaMessage(
+		"no columns defined for update operation in this table",
+		"",
+		[]string{},
+	),
+	XERR_REPO_UPDATE_EXEC_FAILED: xerrors.NewMetaMessage(
+		"failed to execute update statement in the database",
+		"",
+		[]string{},
+	),
+	XERR_REPO_UPDATE_VERIFY_ROWS_FAILED: xerrors.NewMetaMessage(
+		"failed to verify affected rows during update operation",
+		"",
+		[]string{},
+	),
+	XERR_REPO_UPDATE_RECORD_NOT_FOUND: xerrors.NewMetaMessage(
+		"target record not found in database for update operation",
+		"",
+		[]string{},
+	),
+
+	//
+	//
+
+	XERR_REPO_DELETE_EXEC_FAILED: xerrors.NewMetaMessage(
+		"failed to execute delete statement in the database",
+		"",
+		[]string{},
+	),
+	XERR_REPO_DELETE_VERIFY_ROWS_FAILED: xerrors.NewMetaMessage(
+		"failed to verify affected rows during delete operation",
+		"",
+		[]string{},
+	),
+	XERR_REPO_DELETE_RECORD_NOT_FOUND: xerrors.NewMetaMessage(
+		"target record not found in database for delete operation",
+		"",
+		[]string{},
+	),
+
+	//
+	//
+
+	XERR_REPO_GET_BY_ID_EXEC_FAILED: xerrors.NewMetaMessage(
+		"failed to execute select single record query",
+		"",
+		[]string{},
+	),
+	XERR_REPO_GET_BY_ID_RECORD_NOT_FOUND: xerrors.NewMetaMessage(
+		"requested record not found in the database table",
+		"",
+		[]string{},
+	),
+	XERR_REPO_GET_BY_ID_SCAN_FAILED: xerrors.NewMetaMessage(
+		"failed to scan database columns into entity memory pointers",
+		"",
+		[]string{},
+	),
+	XERR_REPO_GET_ALL_EXEC_FAILED: xerrors.NewMetaMessage(
+		"failed to execute select collection list query",
+		"",
+		[]string{},
+	),
+	XERR_REPO_GET_ALL_SCAN_FAILED: xerrors.NewMetaMessage(
+		"failed to scan row iteration into entity memory pointers",
+		"",
+		[]string{},
+	),
+	XERR_REPO_GET_ALL_ITERATION_FAILED: xerrors.NewMetaMessage(
+		"database cursor failure during rows iteration process",
+		"",
+		[]string{},
+	),
+	XERR_REPO_GET_BY_FIELD_EXEC_FAILED: xerrors.NewMetaMessage(
+		"failed to execute field query statement in the database",
+		"",
+		[]string{},
+	),
+	XERR_REPO_GET_BY_FIELD_SCAN_FAILED: xerrors.NewMetaMessage(
+		"failed to scan database row into the entity fields",
+		"",
+		[]string{},
+	),
+	XERR_REPO_GET_WHERE_ARGS_MISMATCH: xerrors.NewMetaMessage(
+		"query parameters count does not match the provided arguments length",
+		"",
+		[]string{},
+	),
+	XERR_REPO_GET_WHERE_EXEC_FAILED: xerrors.NewMetaMessage(
+		"failed to execute conditional query statement in the database",
+		"",
+		[]string{},
+	),
+	XERR_REPO_GET_WHERE_SCAN_FAILED: xerrors.NewMetaMessage(
+		"failed to scan conditional database row into the entity fields",
+		"",
+		[]string{},
+	),
+
+	//
+	//
+
+	XERR_REPO_QUERY_RAW_EXEC_FAILED: xerrors.NewMetaMessage(
+		"failed to execute raw sql query statement in the database",
+		"",
+		[]string{},
+	),
+	XERR_REPO_QUERY_RAW_SCAN_FAILED: xerrors.NewMetaMessage(
+		"failed to scan database row into the raw query destination structure",
+		"",
+		[]string{},
+	),
 }
 
 func init() {
 	xerrors.RegisterDomainErrors(XERR_PKGCTX, xerrorDomainMapRegistry)
 }
-
-// ErrorMessages acts as a centralized English translation catalog for core repository errors.
-var ErrorMessages = map[xerrors.ErrorCode]string{
-	XERR_REPO_INSERT_WITH_UNEXPECTED_NUMERIC_PK: "cannot insert entity with an existing numerical ID",
-	XERR_REPO_INSERT_WITH_UNEXPECTED_STRING_PK:  "cannot insert entity with an existing primary key string",
-	XERR_REPO_INSERT_WITH_UNEXPECTED_EMPTY_PK:   "cannot insert entity: natural primary key string cannot be empty",
-	XERR_REPO_INSERT_WITH_UNEXPECTED_NIL_PK:     "cannot insert entity: natural primary key cannot be nil",
-	XERR_REPO_INSERT_EXECUTION_FAILED:           "failed to execute insert statement in the database",
-	XERR_REPO_INSERT_FETCH_ID_FAILED:            "failed to retrieve last inserted numerical ID from database",
-	XERR_REPO_UPDATE_INVALID_NUMERICAL_PK:       "cannot update entity with invalid numerical ID (must be > 0)",
-	XERR_REPO_UPDATE_EMPTY_STRING_PK:            "cannot update entity with empty string key",
-	XERR_REPO_UPDATE_PK_NIL:                     "cannot update entity without a primary key",
-	XERR_REPO_UPDATE_UNKNOWN_PK_TYPE:            "unknown primary key type format",
-	XERR_REPO_UPDATE_NO_COLUMNS_DEFINED:         "no columns defined for update operation in this table",
-	XERR_REPO_UPDATE_EXEC_FAILED:                "failed to execute update statement in the database",
-	XERR_REPO_UPDATE_VERIFY_ROWS_FAILED:         "failed to verify affected rows during update operation",
-	XERR_REPO_UPDATE_RECORD_NOT_FOUND:           "target record not found in database for update operation",
-	XERR_REPO_DELETE_EXEC_FAILED:                "failed to execute delete statement in the database",
-	XERR_REPO_DELETE_VERIFY_ROWS_FAILED:         "failed to verify affected rows during delete operation",
-	XERR_REPO_DELETE_RECORD_NOT_FOUND:           "target record not found in database for delete operation",
-	XERR_REPO_INTERFACE_ASSERTION_FAILED:        "internal error: entity type does not implement database dao interface",
-	XERR_REPO_GET_BY_ID_EXEC_FAILED:             "failed to execute select single record query",
-	XERR_REPO_GET_BY_ID_RECORD_NOT_FOUND:        "requested record not found in the database table",
-	XERR_REPO_GET_BY_ID_SCAN_FAILED:             "failed to scan database columns into entity memory pointers",
-	XERR_REPO_GET_ALL_EXEC_FAILED:               "failed to execute select collection list query",
-	XERR_REPO_GET_ALL_SCAN_FAILED:               "failed to scan row iteration into entity memory pointers",
-	XERR_REPO_GET_ALL_ITERATION_FAILED:          "database cursor failure during rows iteration process",
-	XERR_REPO_GET_BY_FIELD_EXEC_FAILED:          "failed to execute field query statement in the database",
-	XERR_REPO_GET_BY_FIELD_SCAN_FAILED:          "failed to scan database row into the entity fields",
-	XERR_REPO_GET_WHERE_ARGS_MISMATCH:           "query parameters count does not match the provided arguments length",
-	XERR_REPO_GET_WHERE_EXEC_FAILED:             "failed to execute conditional query statement in the database",
-	XERR_REPO_GET_WHERE_SCAN_FAILED:             "failed to scan conditional database row into the entity fields",
-	XERR_REPO_QUERY_RAW_EXEC_FAILED:             "failed to execute raw sql query statement in the database",
-	XERR_REPO_QUERY_RAW_SCAN_FAILED:             "failed to scan database row into the raw query destination structure",
-}
-
-// // logRepoError normalizes engine metadata and delegates structured repository failure telemetry to the global xlog framework.
-// func NewXDBError(
-// 	ctx context.Context,
-// 	db *sql.DB,
-// 	errCode xerrors.ErrorCode,
-// 	err error,
-// 	query string,
-// 	pkID int64,
-// 	pkUUID string,
-// 	values []any,
-// ) xerrors.IError500 {
-// 	logInfo := ""
-
-// 	return nil
-
-// 	// logErr, ok := err.(xerrors.IError500)
-// 	// if !ok {
-// 	// 	logErr = xerrors.NewError500(
-// 	// 		XERR_PKGCTX,
-// 	// 		errCode,
-// 	// 		err,
-// 	// 		"",
-// 	// 		"",
-// 	// 	)
-// 	// }
-// 	// logErr = logErr.WithCallerSkip(1)
-
-// 	// resource := RetrieveDbType(db)
-
-// 	// attrs := []slog.Attr{
-// 	// 	slog.String("resource", resource),
-// 	// }
-
-// 	// if query != "" {
-// 	// 	attrs = append(attrs, slog.String("sql_query", query))
-// 	// }
-
-// 	// if len(args) > 0 {
-// 	// 	attrs = append(attrs, slog.Any("args", args))
-// 	// }
-
-// 	// attrs = append(attrs, slog.String("error_cod", string(errCode)))
-// 	// attrs = append(attrs, slog.String("error_str", fmt.Sprintf("%s", errCode)))
-
-// 	// // If the original error implements IError500, adjust its caller skip
-// 	// // so the reported component points to the real origin.
-// 	// de, ok := err.(xerrors.IError500)
-// 	// if ok {
-// 	// 	de = de.WithCallerSkip(1)
-// 	// 	attrs = append(attrs, slog.String("component", de.Component()))
-// 	// }
-
-// 	// if err != nil {
-// 	// 	attrs = append(attrs, slog.String("error", err.Error()))
-// 	// }
-
-// 	// // Emit structured error via slog so that consumer projects can configure
-// 	// // `slog` with an `xlog.LogHandler` to control formatting/outputs.
-// 	// slog.LogAttrs(ctx, slog.LevelError, "operation failure detected", attrs...)
-// }
